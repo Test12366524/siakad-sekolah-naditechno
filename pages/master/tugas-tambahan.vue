51dc513a232dadc72a25e391a2124d3810b9d180
sonice007 <soni.setiawan.it07@gmail.com>
2024-12-31 17:46:54 +0700

    update

---
+++ pages/master/tugas-tambahan.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { VCol, VTextField } from "vuetify/lib/components/index.mjs";
+
+const { confirmDialog } = useCommonStore();
+
+const dialogSave = ref();
+
+const tableRef = ref();
+
+const form = {
+  name: "",
+  status: 1,
+};
+
+onMounted(() => {
+  useApi("auth/me").then(({ data }) => {
+    useApi(`master/tugas-tambahan/${data.role_id}`).then(({ data }) => {
+      if(data == 0){
+        navigateTo(`/not-authorized`);
+      }
+    });
+  });
+});
+</script>
+
+<template>
+  <SaveDialog
+    v-if="tableRef"
+    path="master/tugas-tambahan"
+    title="Tambah Tugas Tambahan"
+    edit-title="Edit Tugas Tambahan"
+    v-slot="{ formData, validationErrors, isEditing }"
+    ref="dialogSave"
+    :default-form="form"
+    :refresh-callback="tableRef.refresh"
+  >
+    <VCol cols="12">
+      <VTextField
+        :error-messages="validationErrors.name"
+        v-model="formData.name"
+        label="Nama"
+      />
+    </VCol>
+
+    <VCol cols="12">
+      <VLabel>Status</VLabel>
+      <VRadioGroup
+        inline
+        v-model="formData.status"
+        :error-messages="validationErrors.status"
+      >
+        <VRadio label="Aktif" :value="1"></VRadio>
+        <VRadio label="Nonaktif" :value="0"></VRadio>
+      </VRadioGroup>
+    </VCol>
+
+  </SaveDialog>
+
+  <VRow>
+    <VCol cols="12">
+      <VCard>
+        <VCardItem>
+          <VRow>
+            <VCol>
+              <VBtn @click="dialogSave.show()" color="primary">
+                <VIcon end icon="ri-add-fill" />
+                Tambah Data
+              </VBtn>
+            </VCol>
+          </VRow>
+        </VCardItem>
+      </VCard>
+    </VCol>
+
+    <VCol cols="12">
+      <AppTable
+        ref="tableRef"
+        title="Data Tugas Tambahan"
+        path="master/tugas-tambahan"
+        :with-actions="true"
+        :headers="[
+          {
+            title: 'Nama',
+            key: 'name',
+            sortable: false,
+          },
+          {
+            title: 'Status',
+            key: 'status_desc',
+            sortable: false,
+          },
+        ]"
+      >
+        <template #actions="{ item, remove }">
+          <div class="d-flex gap-1">
+            <IconBtn
+              @click="dialogSave.show({ ...item, status_desc: undefined })"
+              size="small"
+            >
+              <VIcon icon="ri-pencil-line" />
+            </IconBtn>
+            <IconBtn
+              @click="
+                confirmDialog.show({
+                  title: 'Hapus Tugas Tambahan',
+                  message: `Anda yakin ingin menghapus Tugas Tambahan ${
+                    (item as any).name
+                  }?`,
+                  onConfirm: () => remove((item as any).id),
+                })
+              "
+              size="small"
+            >
+              <VIcon icon="ri-delete-bin-line" />
+            </IconBtn>
+          </div>
+        </template>
+      </AppTable>
+    </VCol>
+  </VRow>
+</template>
