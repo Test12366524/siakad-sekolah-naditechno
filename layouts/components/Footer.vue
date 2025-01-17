7f48ec5a0c0b71078ce45524e6580456864c9a46
sonice007 <soni.setiawan.it07@gmail.com>
2025-01-17 09:57:51 +0700

    update

--- layouts/components/Footer.vue
+++ layouts/components/Footer.vue
@@ -6,7 +6,7 @@
     <div class="d-flex align-center text-base">
       &copy;
       {{ new Date().getFullYear() }}
-      PKU MUI Jakarta
+      naditechno.id
     </div>
     <!-- 👉 Footer: right content -->
     <span
